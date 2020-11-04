79282ad047d067c015ef760184ab9f54add3a077
Will Farris <9190155+WillFarris@users.noreply.github.com>
2020-11-04 01:23:55 -0800

    IRQ is working

--- src/kernel.c
+++ src/kernel.c
@@ -15,9 +15,33 @@ void kernel_main()
     enable_interrupt_controller();
     irq_enable();
 
+#define pause 50000000
+
+    uart_putc(0, '\r');
     while(1)
     {
-        
+        uart_putc(0, 'R');
+        delay(pause);
+        uart_putc(0, 'u');
+        delay(pause);
+
+        uart_putc(0, 'n');
+        delay(pause);
+        uart_putc(0, 'n');
+        delay(pause);
+
+        uart_putc(0, 'i');
+        delay(pause);
+        uart_putc(0, 'n');
+        delay(pause);
+
+        uart_putc(0, 'g');
+        delay(pause);
+        uart_putc(0, '\r');
+        delay(pause);
+        printf("       ");
+        uart_putc(0, '\r');
+
     }
     
 }
