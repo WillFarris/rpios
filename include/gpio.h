f1ea3dfb774ffe7b5d05cc6e61001e6d929732a8
Will Farris <9190155+GetLiquid@users.noreply.github.com>
2020-11-05 19:13:31 -0800

    Minor refactor for modularity

--- include/gpio.h
+++ include/gpio.h
@@ -1,3 +1,11 @@
 #pragma once
 
 #define PBASE 0x3F000000
+
+#define LOCAL_TIMER_ADDR 0x40000024
+
+#define SYS_TIMER_ADDR (PBASE + 0x00003000)
+
+#define AUX_REGS_ADDR (PBASE + 0x00215000)
+
+#define GPIO_ADDR (PBASE + 0x00200000)
--- include/utils.h
+++ include/utils.h
@@ -7,3 +7,4 @@ void put32(u64, u32);
 u32 get32(u64);
 void memzero(u64, u64);
 u32 get_el();
+u64 get_core();
