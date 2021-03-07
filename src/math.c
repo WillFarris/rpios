7a0d5cce3471a86e331ee53af5419b09650cbc04
Will Farris <9190155+GetLiquid@users.noreply.github.com>
2021-03-06 22:39:50 -0800

    some math functions

--- src/math.c
+++ src/math.c
@@ -16,4 +16,48 @@ u64 phi(u64 n)
         if (gcd(i, n) == 1) 
             result++; 
     return result; 
+}
+
+u64 floor_sqrt(u64 x) 
+{ 
+    // Base cases 
+    if (x == 0 || x == 1) 
+    return x; 
+  
+    // Staring from 1, try all numbers until 
+    // i*i is greater than or equal to x. 
+    int i = 1, result = 1; 
+    while (result <= x) 
+    { 
+      i++; 
+      result = i * i; 
+    } 
+    return i - 1; 
+} 
+
+void prime_factors(u64 n) 
+{ 
+    // Print the number of 2s that divide n 
+    while (n%2 == 0) 
+    { 
+        fbprintf("%d ", 2); 
+        n = n/2; 
+    } 
+  
+    // n must be odd at this point.  So we can skip  
+    // one element (Note i = i +2) 
+    for (int i = 3; i <= floor_sqrt(n); i = i+2) 
+    { 
+        // While i divides n, print i and divide n 
+        while (n%i == 0) 
+        { 
+            fbprintf("%d ", i); 
+            n = n/i; 
+        } 
+    } 
+  
+    // This condition is to handle the case when n  
+    // is a prime number greater than 2 
+    if (n > 2) 
+        fbprintf ("%d ", n); 
 } 
