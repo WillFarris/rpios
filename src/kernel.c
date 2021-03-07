35f10d187f2994f7255ae84317ab02f5e889a622
Will Farris <9190155+GetLiquid@users.noreply.github.com>
2021-03-06 22:40:11 -0800

    disable UART IRQ interrupts

--- src/kernel.c
+++ src/kernel.c
@@ -7,110 +7,19 @@
 #include "font.h"
 #include "timer.h"
 #include "regstruct.h"
-#include "string.h"
 #include "math.h"
 
 #include "mem.h"
 #include "reg.h"
 
-#define DISPLAY_WIDTH 1024
-#define DISPLAY_HEIGHT 600;
-
 struct FrameBuffer fb;
-char commandbuffer[DISPLAY_WIDTH];
-char * args[10];
-char *cur;
-
-#define cmd_match(var, cmd) (strcmp(var, cmd) == 0)
-
-void exec()
-{
-    int i=0;
-    while(args[i])
-    {
-        printf("Arg %d: %s\n", i, args[i]);
-        ++i;
-    }
-    if(strcmp(args[0], "gcd") == 0)
-    {
-        u64 a = strtol(args[1]);
-        u64 b = strtol(args[2]);
-        fbprintf("%d\n> ", gcd(a, b));
-    } else if(strcmp(args[0], "phi") == 0)
-    {
-        u64 n = strtol(args[1]);
-        fbprintf("%d\n> ", phi(n));
-    } else if(strcmp(args[0], "clear") == 0)
-    {
-        fbclear(0x800000);
-        fbprintf("> ");
-    } else if(strcmp(args[0], "mod") == 0)
-    {
-        u64 a = strtol(args[1]);
-        u64 b = strtol(args[2]);
-        fbprintf("%d\n> ", a % b);
-    } else
-    {
-        fbprintf("Unknown command: %s\n> ", args[0]);
-    }
-}
-
-void command(char *str)
-{
-    int i=0;
-    char *argstart = str;
-
-    char * cur = str;
-    while(*cur)
-    {
-        if(i > 9) return;
-        if(*cur == ' ')
-        {
-            args[i++] = argstart;
-            *cur = 0;
-            argstart = ++cur;
-        } else ++cur;
-    }
-    args[i++] = argstart;
-    args[i] = 0;
-    exec();
-}
-
-void shell()
-{
-    char c = uart_getc();
-    switch(c)
-    {
-        case 0x7F:
-            fb.cursor_x -= char_width;
-            *(--cur) = 0;
-            --cur;
-            fbputc(' ');
-            fb.cursor_x -= char_width;
-            break;
-        case '\r':
-            fbputc('\n');
-            fbputc('\r');
-            fbputc('>');
-            fbputc(' ');
-            *cur = 0;
-            cur = commandbuffer;
-            command(commandbuffer);
-            break;
-        default:
-            fbputc(c);
-            *cur = c;
-            break;
-    }
-    if(c != '\r') ++cur;
-}
 
 void kernel_main() 
 {
     uart_init_alt();    
     init_printf(0, putc);
 
-    int fb_status = fbinit(1024, 600);
+    int fb_status = fbinit(DISPLAY_WIDTH, DISPLAY_HEIGHT);
     u32 bg = 0x800000;
     fbclear(bg);
 
@@ -133,9 +42,6 @@ void kernel_main()
     
     printf("\nFrameBuffer:\n  width: %d\n  height: %d\n  pitch: %d\n  background: 0x%X\n  address: 0x%X\n", fb.width, fb.height, fb.pitch, fb.bg, fb.ptr);
     
-    fbprintf("\n> ");
-    
-    cur = commandbuffer;
 
-    while(1);
+    shell();
 }
---
+++ src/shell.c
@@ -0,0 +1,107 @@
+#include "shell.h"
+#include "types.h"
+#include "math.h"
+#include "font.h"
+#include "string.h"
+#include "fb.h"
+#include "printf.h"
+
+void exec(char **args)
+{
+    int i=0;
+    while(args[i])
+    {
+        printf("Arg %d: %s\n", i, args[i]);
+        ++i;
+    }
+    if(strcmp(args[0], "gcd") == 0)
+    {
+        u64 a = strtol(args[1]);
+        u64 b = strtol(args[2]);
+        fbprintf("%d\n> ", gcd(a, b));
+    } else if(strcmp(args[0], "phi") == 0)
+    {
+        u64 n = strtol(args[1]);
+        fbprintf("%d\n> ", phi(n));
+    } else if (strcmp(args[0], "primefactors") == 0)
+    {
+        u64 n = strtol(args[1]);
+        prime_factors(n);
+        fbprintf("\n> ");
+    } else if(strcmp(args[0], "clear") == 0)
+    {
+        fbclear(0x800000);
+        fbprintf("> ");
+    } else if(strcmp(args[0], "mod") == 0)
+    {
+        u64 a = strtol(args[1]);
+        u64 b = strtol(args[2]);
+        fbprintf("%d\n> ", a % b);
+    }  else if(strcmp(args[0], "setres") == 0)
+    {
+        u64 w = strtol(args[1]);
+        u64 h = strtol(args[2]);
+        fbsetres(w, h);
+    } else
+    {
+        fbprintf("Unknown command: %s\n> ", args[0]);
+    }
+}
+
+void parse_command(char *str, char **args)
+{
+    int i=0;
+    char *argstart = str;
+
+    char * cur = str;
+    while(*cur)
+    {
+        if(i > 9) return;
+        if(*cur == ' ')
+        {
+            args[i++] = argstart;
+            *cur = 0;
+            argstart = ++cur;
+        } else ++cur;
+    }
+    args[i++] = argstart;
+    args[i] = 0;
+    exec(args);
+}
+
+void shell()
+{
+    char commandbuffer[DISPLAY_WIDTH];
+    char * args[10];
+    char *cur = commandbuffer;
+
+    fbprintf("\n> ");
+    while(1)
+    {
+        char c = uart_getc();
+        switch(c)
+        {
+            case 0x7F:
+                fb.cursor_x -= char_width;
+                *(--cur) = 0;
+                --cur;
+                fbputc(' ');
+                fb.cursor_x -= char_width;
+                break;
+            case '\r':
+                fbputc('\n');
+                fbputc('\r');
+                fbputc('>');
+                fbputc(' ');
+                *cur = 0;
+                cur = commandbuffer;
+                parse_command(commandbuffer, args);
+                break;
+            default:
+                fbputc(c);
+                *cur = c;
+                break;
+        }
+        if(c != '\r') ++cur;
+    }
+}
