29ce702a851d3b9400a47d07fb70c85550b6293d
Will Farris <9190155+WillFarris@users.noreply.github.com>
2021-09-26 01:31:00 -0700

    work on virtual memory

--- headers/mm.h
+++ headers/mm.h
@@ -2,12 +2,19 @@
 
 #include "types.h"
 
-//extern const u64 __kernel_heap_size;
 
 #define PAGE_SIZE 4096
 #define NUM_PAGES 16777216/PAGE_SIZE
-//#define NUM_PAGES __kernel_heap_size / PAGE_SIZE
+
 
 u64 get_free_page();
 void free_page(void *);
+void mmu_init_asm();
 void mmu_init();
+u64 get_id_aa64mmfr0_el1();
+
+// SHIFT = log2(size)
+// NUM_LVL2_TABLES = KernelAddressSpaceSize >> Granule512MiBShift
+//                 = (0x4000_FFFF + 1) >> log2()
+#define NUM_TABLES ((0x4000FFFF + 1) >> 30)
+
